48c5ca1fcfc53ba268e93ad12cbe274f399c436f
Michael A Bertagna <bertagna@chapman.edu>
2022-08-03 10:22:27 -0700

    Add Github Actions CI workflow

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coralign',
+    version='1.0.0',
+    author='A J Eldorado Riggs',
+    description='Package for the alignment and calibration of coronagraph optics',
+    url='https://github.com/nasa-jpl/coralign',
+    packages=find_packages(),
+)
